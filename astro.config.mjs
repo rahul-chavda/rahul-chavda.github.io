3bf97edf440eb51b0d000c7dc6e884deafc07e9d
Rahul Chavda <rchavda88@gmail.com>
2025-04-18 19:01:32 +0530

    Refactor layout, add lab section, and implement globe feature.
    Reorganized component structure with aliases in `tsconfig.json`.
    Added a new "lab" section featuring a 3D interactive globe built using Three.js and Vue.
    Removed `.vscode` settings and updated dependencies to include Vue and Three.js libraries.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rahul-chavda.github.io/',
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [mdx(), sitemap(), icon(), vue()],
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ src/components/lab/globe/GlobeComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { type Ref } from 'vue';
+import { GlobeVisualisation } from "./GlobeVisualisation.ts";
+
+const sceneCanvas: Ref<HTMLCanvasElement | null> = ref(null);
+
+let globeVisualisation: GlobeVisualisation;
+
+onMounted(() => {
+  globeVisualisation = new GlobeVisualisation(sceneCanvas.value!);
+  globeVisualisation.init();
+});
+
+onBeforeUnmount(() => {
+  globeVisualisation.dispose();
+});
+
+</script>
+
+<template>
+  <canvas ref="sceneCanvas"></canvas>
+</template>
